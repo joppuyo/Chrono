40e0acbfd059e2058dd4f2fa88693f3448a07700
Johannes Siipola <joppu@kapsi.fi>
2015-05-31 15:19:52 +0300

    Time formatting and start-stop logic

--- js/script.js
+++ js/script.js
@@ -1,28 +1,51 @@
 $(function(){
 
+    var counterInterval;
+
+    countdown.setLabels(
+        'ms|s|m|h|d|w|m|y|||',
+        'ms|s|m|h|d|w|m|y|||',
+        '',
+        '',
+        '');
+
     if (localStorage.active === "true") {
         startCounter();
     }
 
     $("#circle").click(function(){
-        localStorage.active = "true";
-        var dateTime = moment();
-        alert(dateTime);
-        localStorage.setItem("startTime", dateTime.format());
-        startCounter();
+        if (localStorage.active !== "true") {
+            localStorage.active = "true";
+            var dateTime = moment();
+            localStorage.setItem("startTime", dateTime.format());
+            startCounter();
+        }
     });
 
     function startCounter(){
         updateCounter();
-        setInterval(updateCounter, 1000);
+        counterInterval = setInterval(updateCounter, 1000);
         $("#circle").addClass("circle-active");
+        $("#action-finish").toggleClass("hidden");
+    }
+
+    function stopCounter(){
+        $("#circle").removeClass("circle-active");
+        $("#circle").text("Start");
+        $("#action-finish").toggleClass("hidden");
+        localStorage.active = "false";
+        clearInterval(counterInterval);
     }
 
     function updateCounter(){
         var startDateTime = moment(localStorage.startTime);
-        var nowDateTime = moment();
-        var secs = nowDateTime.diff(startDateTime, "seconds");
-        $("#circle").text(secs + "s");
+        var time = countdown( startDateTime.toDate() ).toString();
+        $("#circle").text(time);
     }
+
+    $("#action-finish").click(function(){
+        alert("send data to server");
+        stopCounter();
+    });
 
 });
